1e9f0a49c1500b1039fda04a8e64631072a7333e
AlexC163 <95631939+AlexC163@users.noreply.github.com>
2022-10-29 20:34:37 -0500

    Create MainTest.java

---
+++ src/MainTest.java
@@ -0,0 +1,61 @@
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+	@Test
+	void testAddingFamiles() throws MaxFamilySizeExceeded {
+		Main main = new Main();
+		main.addNewFamilyToQueue(3);
+		main.addNewFamilyToQueue(8);
+		main.addNewFamilyToQueue(4);
+		
+		assertEquals("Family Sizes: 3, 8, 4", main.printFamilies());
+	}
+	
+	@Test
+	void testAddingFamilesFail() throws MaxFamilySizeExceeded {
+		Main main = new Main();
+		main.addNewFamilyToQueue(3);
+		main.addNewFamilyToQueue(8);
+		main.addNewFamilyToQueue(4);
+		
+		assertThrows(MaxFamilySizeExceeded.class, () -> main.addNewFamilyToQueue(100));
+	}
+	
+	@Test
+	void testFindsLargestFamily() throws MaxFamilySizeExceeded {
+		Main main = new Main();
+		main.addNewFamilyToQueue(3);
+		main.addNewFamilyToQueue(8);
+		main.addNewFamilyToQueue(4);
+		
+		assertEquals(8, main.findLargestFamily());
+	}
+	
+	@Test
+	void testSortingFamilies() throws MaxFamilySizeExceeded {
+		Main main = new Main();
+		main.addNewFamilyToQueue(7);
+		main.addNewFamilyToQueue(1);
+		main.addNewFamilyToQueue(9);
+		
+		main.insertionSort();
+		
+		assertEquals("Family Sizes: 1, 7, 9", main.printFamilies());
+	}
+
+	@Test
+	void testPrinting() throws MaxFamilySizeExceeded {
+		Main main = new Main();
+		main.addNewFamilyToQueue(5);
+		main.addNewFamilyToQueue(9);
+		main.addNewFamilyToQueue(2);
+				
+		assertEquals("Family Sizes: 5, 9, 2", main.printFamilies());
+	}
+	
+	
+}
